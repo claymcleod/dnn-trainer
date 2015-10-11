cba7aef460f28ed9e9348df4c644e9c290ffd130
Clay McLeod <clay.l.mcleod@gmail.com>
2015-10-11 11:08:29 -0500

    latest

--- configure.sh
+++ configure.sh
@@ -1,1 +1,1 @@
-ssh cc@$1 'sudo yum upgrade -y && sudo yum install git && git clone https://github.com/claymcleod/dnn-trainer.git && cd ./dnn-trainer/docker-config && make'
+ssh cc@$1 'sudo yum upgrade -y && sudo yum install git docker -y && git clone https://github.com/claymcleod/dnn-trainer.git && cd ./dnn-trainer/docker-config && make'
