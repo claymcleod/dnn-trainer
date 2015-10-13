0292b6f84279830e0bd264c2d7a6e0681c726d5e
Clay McLeod <clay.l.mcleod@gmail.com>
2015-10-13 13:21:58 -0500

    latest

---
+++ start-all-servers.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+(./start-server.sh 129.114.108.225) &
+(./start-server.sh 129.114.108.226) &
+(./start-server.sh 129.114.108.227) &
+(./start-server.sh 129.114.108.228) &
+(./start-server.sh 129.114.108.229) &
---
+++ stop-all-servers.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+(./stop-server.sh 129.114.108.225) &
+(./stop-server.sh 129.114.108.226) &
+(./stop-server.sh 129.114.108.227) &
+(./stop-server.sh 129.114.108.228) &
+(./stop-server.sh 129.114.108.229) &
